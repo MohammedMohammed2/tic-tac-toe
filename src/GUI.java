5380839c9ec8a2bfd90f771379188a51fe675354
MohammedMohammed2 <mohammed.mohammed1@gritacademy.se>
2023-11-20 19:52:48 +0100

    added some things

--- src/GUI.java
+++ src/GUI.java
@@ -1,8 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class GUI {
+    boolean playerX;
+    boolean PlayerO;
     private JButton [] buttons = new JButton[9];
     GUI(){
         JFrame frame = new JFrame();
@@ -25,16 +29,56 @@ public class GUI {
     private JPanel PlayingField(){
         JPanel Field = new JPanel(new BorderLayout(10,10));
         Field.setBorder(BorderFactory.createLineBorder(Color.black,2));
-        Field.setPreferredSize(new Dimension(300,300));
+        Field.setPreferredSize(new Dimension(400,400));
         JPanel panel = new JPanel(new GridLayout(3, buttons.length,10,10));
         for(int i = 0; i<buttons.length;i++) {
             buttons[i] = new JButton();
+            buttons[i].addActionListener(new EventListener());
+            buttons[i].setActionCommand(i+"");
             panel.add(buttons[i]);
         }
         Field.add(panel);
         return Field;
-        hejhejhej
-
     }
-
+public class EventListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("0")) {
+            buttons[0].setText("x");
+            buttons[0].setEnabled(false);
+        }
+        if (e.getActionCommand().equals("1")) {
+            buttons[1].setText("x");
+            buttons[1].setEnabled(false);
+        }
+        if (e.getActionCommand().equals("2")) {
+            buttons[2].setText("x");
+            buttons[2].setEnabled(false);
+        }
+        if (e.getActionCommand().equals("3")) {
+            buttons[3].setText("x");
+            buttons[3].setEnabled(false);
+        }
+        if (e.getActionCommand().equals("4")) {
+            buttons[4].setText("O");
+            buttons[4].setEnabled(false);
+        }
+        if (e.getActionCommand().equals("5")) {
+            buttons[5].setText("x");
+            buttons[5].setEnabled(false);
+        }
+        if (e.getActionCommand().equals("6")) {
+            buttons[6].setText("O");
+            buttons[6].setEnabled(false);
+        }
+        if (e.getActionCommand().equals("7")) {
+            buttons[7].setText("O");
+            buttons[7].setEnabled(false);
+        }
+        if (e.getActionCommand().equals("8")) {
+            buttons[8].setText("O");
+            buttons[8].setEnabled(false);
+        }
+    }
+}
 }
